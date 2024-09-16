4acc98537889ade0eb79143229d5cc54a738a21c
Nisharg-K <2203031280012@paruluniversity.acc.in>
2024-09-16 19:30:02 +0530

    Version 1.0: Locally 2 player can play

--- bicbacboe/script.js
+++ bicbacboe/script.js
@@ -17,9 +17,9 @@ document.addEventListener("DOMContentLoaded", () => {
         ['3', '5', '7']
     ];
 
-    const winnerPopup = document.getElementById("winnerPopup");
-    const winnerText = document.getElementById("winnerText");
-    const closeBtn = document.querySelector(".close-btn");
+    const scorePopup = document.getElementById("scorePopup");
+    const scoreMessage = document.getElementById("scoreMessage");
+    const closeBtn = document.getElementById("closePopup");
 
     function checkWinner(moves) {
         return winningCombinations.some(combination => 
@@ -28,49 +28,42 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function showPopup(winner) {
-        winnerText.innerText = `${winner} Wins!`;
-        winnerPopup.classList.add("show");
+        scoreMessage.innerText = `${winner} Wins!`;
+        scorePopup.classList.add("show");
     }
 
     function closePopup() {
-        winnerPopup.classList.remove("show");
-        setTimeout(() => {
-            winnerPopup.style.display = "none";
-        }, 300); // Match this duration with CSS transition
+        scorePopup.classList.remove("show");
     }
 
     closeBtn.addEventListener("click", closePopup);
 
     boxes.forEach((box) => {
         box.addEventListener("click", () => {
-            console.log("CLICK!", box.id);
-            if (xturn) {
-                xturn = false;
-                box.innerText = "X";
-                xmoves.push(box.id);
-                console.log("X's Moves:", xmoves);
-
-                if (checkWinner(xmoves)) {
-                    showPopup("X");
-                    boxes.forEach(box => box.disabled = true); // Disable all boxes
+            if (box.innerText === "") {
+                if (xturn) {
+                    box.innerText = "X";
+                    xmoves.push(box.id);
+                    if (checkWinner(xmoves)) {
+                        showPopup("Player X");
+                        boxes.forEach(box => box.disabled = true); // Disable all boxes
+                    }
+                    xturn = false;
+                } else {
+                    box.innerText = "O";
+                    ymoves.push(box.id);
+                    if (checkWinner(ymoves)) {
+                        showPopup("Player O");
+                        boxes.forEach(box => box.disabled = true); // Disable all boxes
+                    }
+                    xturn = true;
                 }
-            } else {
-                xturn = true;
-                box.innerText = "O";
-                ymoves.push(box.id);
-                console.log("Y's Moves:", ymoves);
-
-                if (checkWinner(ymoves)) {
-                    showPopup("O");
-                    boxes.forEach(box => box.disabled = true); // Disable all boxes
-                }
+                box.disabled = true;
             }
-            box.disabled = true;
         });
     });
 
     reset.addEventListener("click", () => {
-        // Reset the game
         xturn = true;
         xmoves = [];
         ymoves = [];
